36df51131f64df22cc5975f073941df1d89dbb02
27253 <user.email>
2026-02-07 20:59:16 +0000

    early none usable update

--- main.py
+++ main.py
@@ -2,4 +2,15 @@ import sys
 import os
 
 def printMenu():
+    print("\nSelecione uma das seguintes opções:")
+    print("1. Port Scanner")
+
+def main():
+    printMenu()
+    while True:
+        try:
+            choice = input("\n Opção: ")
+        except(KeyboardInterrupt, EOFError):
+            print("\n\n Programa terminado")
+
 
